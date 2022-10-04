8cefccede7a7ada7439ec201c9ec6eb38daf96b3
LeoUribC <leo.uribe1205@gmail.com>
2022-10-04 15:31:15 -0500

    User class creatred

---
+++ prueba_SOLID/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba_SOLID.Models
+{
+    public class User
+    {
+        public string Username { get; }
+        public string Name { get; set; }
+
+        public string Password { get; }
+
+        private bool _validCredentials = false;
+
+        
+
+        public bool IsAuthenticated
+        {
+            get
+            {
+                return _validCredentials;
+            }
+        }
+
+        public User(string username, string name,  string password)
+        {
+            Username = username;
+            Name = name;
+           Password = password;
+            
+        }
+
+        public void Authenticate()
+        {
+            // TODO: Validate with database data
+            if (_validCredentials)
+            {
+                throw new Exception("El usuario ya está autenticado");
+            }
+            
+        }
+    }
+}
